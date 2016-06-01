a6df630195f74033f82b8f387ddaa5ee549710c0
Yad Smood <y.s.inside@gmail.com>
2016-06-01 19:05:53 +0800

    upd

--- test/socket.js
+++ test/socket.js
@@ -52,262 +52,262 @@ module.exports = (it) => {
             return defer.promise;
         }));
 
-        it('client call server', async(function * (after) {
-            var defer = kit.Deferred();
-            var client;
+        // it('client call server', async(function * (after) {
+        //     var defer = kit.Deferred();
+        //     var client;
 
-            after(() => {
-                client.close();
-                server.close();
-            });
+        //     after(() => {
+        //         client.close();
+        //         server.close();
+        //     });
 
-            var server = nisper({
-                socketOptions: { port: 0 },
-                sandbox: {
-                    echo: fn((msg) => kit.sleep(30, msg))
-                }
-            });
+        //     var server = nisper({
+        //         socketOptions: { port: 0 },
+        //         sandbox: {
+        //             echo: fn((msg) => kit.sleep(30, msg))
+        //         }
+        //     });
 
-            var httpServer = server.server._server;
+        //     var httpServer = server.server._server;
 
-            httpServer.on('listening', () => {
-                client = nisper({
-                    url: `ws://127.0.0.1:${httpServer.address().port}`,
-                    onOpen: () => {
-                        client.call(['echo', 'hi']).then((msg) => {
-                            defer.resolve(it.eq(msg, 'hi'));
-                        });
-                    }
-                });
-            });
+        //     httpServer.on('listening', () => {
+        //         client = nisper({
+        //             url: `ws://127.0.0.1:${httpServer.address().port}`,
+        //             onOpen: () => {
+        //                 client.call(['echo', 'hi']).then((msg) => {
+        //                     defer.resolve(it.eq(msg, 'hi'));
+        //                 });
+        //             }
+        //         });
+        //     });
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
-        it('client call server once', async(function * (after) {
-            var defer = kit.Deferred();
+        // it('client call server once', async(function * (after) {
+        //     var defer = kit.Deferred();
 
-            after(() => {
-                server.close();
-            });
+        //     after(() => {
+        //         server.close();
+        //     });
 
-            var server = nisper({
-                socketOptions: { port: 0 },
-                sandbox: {
-                    echo: fn((msg) => {
-                        return msg;
-                    })
-                }
-            });
+        //     var server = nisper({
+        //         socketOptions: { port: 0 },
+        //         sandbox: {
+        //             echo: fn((msg) => {
+        //                 return msg;
+        //             })
+        //         }
+        //     });
 
-            var httpServer = server.server._server;
+        //     var httpServer = server.server._server;
 
-            httpServer.on('listening', () => {
-                nisperCall(
-                    `ws://127.0.0.1:${httpServer.address().port}`,
-                    ['echo', 'hi']
-                )
-                .then((msg) => {
-                    defer.resolve(it.eq(msg, 'hi'));
-                });
-            });
+        //     httpServer.on('listening', () => {
+        //         nisperCall(
+        //             `ws://127.0.0.1:${httpServer.address().port}`,
+        //             ['echo', 'hi']
+        //         )
+        //         .then((msg) => {
+        //             defer.resolve(it.eq(msg, 'hi'));
+        //         });
+        //     });
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
-        it('async env', async(function * (after) {
-            var app = flow();
-            yield app.listen(0);
-            var defer = kit.Deferred();
+        // it('async env', async(function * (after) {
+        //     var app = flow();
+        //     yield app.listen(0);
+        //     var defer = kit.Deferred();
 
-            after(() => {
-                client.close();
-                app.close();
-            });
+        //     after(() => {
+        //         client.close();
+        //         app.close();
+        //     });
 
-            var server = nisper({
-                httpServer: app.server,
-                onOpen: () => kit.sleep(30, 'ok'),
-                sandbox: {
-                    echo: fn(function (msg) {
-                        defer.resolve(it.eq(this + msg, 'ok!'));
-                    })
-                }
-            });
+        //     var server = nisper({
+        //         httpServer: app.server,
+        //         onOpen: () => kit.sleep(30, 'ok'),
+        //         sandbox: {
+        //             echo: fn(function (msg) {
+        //                 defer.resolve(it.eq(this + msg, 'ok!'));
+        //             })
+        //         }
+        //     });
 
-            var client = nisper({
-                url: `ws://127.0.0.1:${app.server.address().port}`,
-                onOpen: () => {
-                    client.call(['echo', '!']);
-                }
-            });
+        //     var client = nisper({
+        //         url: `ws://127.0.0.1:${app.server.address().port}`,
+        //         onOpen: () => {
+        //             client.call(['echo', '!']);
+        //         }
+        //     });
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
-        it('client call server multiple times', async(function * (after) {
-            var app = flow();
-            yield app.listen(0);
-            var defer = kit.Deferred();
+        // it('client call server multiple times', async(function * (after) {
+        //     var app = flow();
+        //     yield app.listen(0);
+        //     var defer = kit.Deferred();
 
-            after(() => {
-                client.close();
-                app.close();
-            });
+        //     after(() => {
+        //         client.close();
+        //         app.close();
+        //     });
 
-            var count = 0;
+        //     var count = 0;
 
-            var server = nisper({
-                httpServer: app.server,
-                sandbox: {
-                    echo: fn(() => {
-                        count++;
-                        if (count === 100)
-                            defer.resolve();
-                    })
-                }
-            });
+        //     var server = nisper({
+        //         httpServer: app.server,
+        //         sandbox: {
+        //             echo: fn(() => {
+        //                 count++;
+        //                 if (count === 100)
+        //                     defer.resolve();
+        //             })
+        //         }
+        //     });
 
-            var client = nisper({
-                url: `ws://127.0.0.1:${app.server.address().port}`,
-                onOpen: () => {
-                    for (var i = 0; i < 100; i++) {
-                        client.call(['echo']);
-                    }
-                }
-            });
+        //     var client = nisper({
+        //         url: `ws://127.0.0.1:${app.server.address().port}`,
+        //         onOpen: () => {
+        //             for (var i = 0; i < 100; i++) {
+        //                 client.call(['echo']);
+        //             }
+        //         }
+        //     });
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
-        it('client call server wait connection', async(function * (after) {
-            var app = flow();
-            yield app.listen(0);
-            var defer = kit.Deferred();
+        // it('client call server wait connection', async(function * (after) {
+        //     var app = flow();
+        //     yield app.listen(0);
+        //     var defer = kit.Deferred();
 
-            after(() => {
-                client.close();
-                app.close();
-            });
+        //     after(() => {
+        //         client.close();
+        //         app.close();
+        //     });
 
-            var count = 0;
+        //     var count = 0;
 
-            var server = nisper({
-                httpServer: app.server,
-                sandbox: {
-                    echo: fn(() => {
-                        if (++count === 3)
-                            defer.resolve();
-                    })
-                }
-            });
+        //     var server = nisper({
+        //         httpServer: app.server,
+        //         sandbox: {
+        //             echo: fn(() => {
+        //                 if (++count === 3)
+        //                     defer.resolve();
+        //             })
+        //         }
+        //     });
 
-            var client = nisper({
-                url: `ws://127.0.0.1:${app.server.address().port}`
-            });
+        //     var client = nisper({
+        //         url: `ws://127.0.0.1:${app.server.address().port}`
+        //     });
 
-            yield client.call(['echo']);
-            yield client.call(['echo']);
-            yield client.call(['echo']);
+        //     yield client.call(['echo']);
+        //     yield client.call(['echo']);
+        //     yield client.call(['echo']);
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
-        it('server call client error', async(function * (after) {
-            var app = flow();
-            yield app.listen(0);
-            var defer = kit.Deferred();
+        // it('server call client error', async(function * (after) {
+        //     var app = flow();
+        //     yield app.listen(0);
+        //     var defer = kit.Deferred();
 
-            after(() => {
-                client.close();
-                app.close();
-            });
+        //     after(() => {
+        //         client.close();
+        //         app.close();
+        //     });
 
-            var server = nisper({
-                httpServer: app.server,
-                onOpen: () => {
-                    server.call(['echo', 'hi']).catch((err) => {
-                        defer.resolve(it.eq(JSON.parse(err.message).message, 'err'));
-                    });
-                }
-            });
+        //     var server = nisper({
+        //         httpServer: app.server,
+        //         onOpen: () => {
+        //             server.call(['echo', 'hi']).catch((err) => {
+        //                 defer.resolve(it.eq(JSON.parse(err.message).message, 'err'));
+        //             });
+        //         }
+        //     });
 
-            var client = nisper({
-                url: `ws://127.0.0.1:${app.server.address().port}`,
-                sandbox: {
-                    echo: fn((msg) => {
-                        throw 'err';
-                    })
-                }
-            });
+        //     var client = nisper({
+        //         url: `ws://127.0.0.1:${app.server.address().port}`,
+        //         sandbox: {
+        //             echo: fn((msg) => {
+        //                 throw 'err';
+        //             })
+        //         }
+        //     });
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
-        it('client call server error', async(function * (after) {
-            var app = flow();
-            yield app.listen(0);
-            var defer = kit.Deferred();
+        // it('client call server error', async(function * (after) {
+        //     var app = flow();
+        //     yield app.listen(0);
+        //     var defer = kit.Deferred();
 
-            after(() => {
-                client.close();
-                app.close();
-            });
+        //     after(() => {
+        //         client.close();
+        //         app.close();
+        //     });
 
-            var server = nisper({
-                httpServer: app.server,
-                sandbox: {
-                    echo: fn((msg) => {
-                        throw 'err';
-                    })
-                }
-            });
+        //     var server = nisper({
+        //         httpServer: app.server,
+        //         sandbox: {
+        //             echo: fn((msg) => {
+        //                 throw 'err';
+        //             })
+        //         }
+        //     });
 
-            var client = nisper({
-                url: `ws://127.0.0.1:${app.server.address().port}`,
-                onOpen: () => {
-                    client.call(['echo', 'hi']).catch((err) => {
-                        defer.resolve(it.eq(JSON.parse(err.message).message, 'err'));
-                    });
-                }
-            });
+        //     var client = nisper({
+        //         url: `ws://127.0.0.1:${app.server.address().port}`,
+        //         onOpen: () => {
+        //             client.call(['echo', 'hi']).catch((err) => {
+        //                 defer.resolve(it.eq(JSON.parse(err.message).message, 'err'));
+        //             });
+        //         }
+        //     });
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
-        it('msgpack', async(function * (after) {
-            var msgpack = require('msgpack-js');
-            var app = flow();
-            yield app.listen(0);
-            var defer = kit.Deferred();
+        // it('msgpack', async(function * (after) {
+        //     var msgpack = require('msgpack-js');
+        //     var app = flow();
+        //     yield app.listen(0);
+        //     var defer = kit.Deferred();
 
-            after(() => {
-                client.close();
-                app.close();
-            });
+        //     after(() => {
+        //         client.close();
+        //         app.close();
+        //     });
 
-            var server = nisper({
-                httpServer: app.server,
-                encode: msgpack.encode,
-                decode: msgpack.decode,
-                onOpen: () => {
-                    server.call(['echo', new Buffer([0, 1, 2, 3])]);
-                }
-            });
+        //     var server = nisper({
+        //         httpServer: app.server,
+        //         encode: msgpack.encode,
+        //         decode: msgpack.decode,
+        //         onOpen: () => {
+        //             server.call(['echo', new Buffer([0, 1, 2, 3])]);
+        //         }
+        //     });
 
-            var client = nisper({
-                url: `ws://127.0.0.1:${app.server.address().port}`,
-                encode: msgpack.encode,
-                decode: msgpack.decode,
-                sandbox: {
-                    echo: fn((msg) => {
-                        defer.resolve(it.eq(msg, [0, 1, 2, 3]));
-                    })
-                }
-            });
+        //     var client = nisper({
+        //         url: `ws://127.0.0.1:${app.server.address().port}`,
+        //         encode: msgpack.encode,
+        //         decode: msgpack.decode,
+        //         sandbox: {
+        //             echo: fn((msg) => {
+        //                 defer.resolve(it.eq(msg, [0, 1, 2, 3]));
+        //             })
+        //         }
+        //     });
 
-            return defer.promise;
-        }));
+        //     return defer.promise;
+        // }));
 
     });
 };
