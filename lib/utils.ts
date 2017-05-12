ca92cda27d6275a1ed2bbb8cc8052dc700160c0e
Yad Smood <y.s.inside@gmail.com>
2017-05-12 19:41:46 +0800

    opt: the security of id algorithm

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,10 @@ export function extend (to, from) {
     return to;
 }
 
-export function genId () {
-    return Math.floor(Math.random() * 100000000).toString(36);
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(36).substring(1)
 }
+export function genId (): string {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
+}
+
